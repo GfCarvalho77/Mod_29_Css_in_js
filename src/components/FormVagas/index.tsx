736a4ab7c7e6655c9216fe7a09c38979e82b2596
GfCarvalho77 <127401543+GfCarvalho77@users.noreply.github.com>
2024-09-06 22:47:01 -0300

    Commit inicial do projeto

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useState } from 'react'
-
-import styles from './FormVagas.module.css'
+import { Formulario, InputCampo, BotaoPesquisa } from './styles'
+// import styles from './FormVagas.module.css'
 
 type Props = {
   aoPesquisar: (termo: string) => void
@@ -15,17 +15,15 @@ const FormVagas = ({ aoPesquisar }: Props) => {
   }
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <Formulario onSubmit={aoEnviarForm}>
+      <InputCampo
+        // className={styles.campo}
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
         type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
-        Pesquisar
-      </button>
-    </form>
+      <BotaoPesquisa type="submit">Pesquisar</BotaoPesquisa>
+    </Formulario>
   )
 }
 export default FormVagas
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import EstiloGlobal from './styles'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <EstiloGlobal />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>
 )
 
 // If you want to start measuring performance in your app, pass a function
